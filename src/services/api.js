62b9c580b126425132206d9336efd0843cf12c11
Raj Kumar Kuncham <rajkumar.kuncham@valuemomentum.com>
2026-05-15 14:17:19 +0530

    src updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080'
+  baseURL: 'http://aeb7e038165c449d789c27495443828d-753465246.ap-south-1.elb.amazonaws.com'
 });
 
 export const getProducts = () => {
   return API.get('/products');
 };
+
+export default API;
